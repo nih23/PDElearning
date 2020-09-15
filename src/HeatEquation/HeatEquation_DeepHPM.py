699ad2b79c890ea10a530c0d8ad67c8a3d074300
nih23 <nico.hoffmann@tu-dresden.de>
2020-09-15 17:28:54 +0200

    fixed several bugs relating to preprocessing of UKD data

---
+++ src/HeatEquation/HeatEquation_DeepHPM.py
@@ -0,0 +1,360 @@
+import numpy as np
+import time
+import torch
+import torch.nn as nn
+import torch.autograd
+import h5py
+import torch.optim as optim
+import scipy.io
+from torch.autograd import Variable
+import torch.optim as optim
+from enum import Enum
+from sklearn.linear_model import LinearRegression
+
+from UKDDataset import HeatEquationHPMDataset
+from HeatEquation_baseline_nohvd import  writeIntermediateState, valLoss, save_checkpoint, load_checkpoint, writeValidationLoss, HeatEquationBaseNet
+
+import matplotlib.pyplot as plt
+import torch.utils.data.distributed
+import horovod.torch as hvd
+from tensorboardX import SummaryWriter
+from argparse import ArgumentParser
+import os
+import sys
+import pathlib
+import torch.nn.functional as F
+
+import os
+import h5py
+
+class HeatEquationHPMNet(HeatEquationBaseNet):
+    def __init__(self, numLayers, numFeatures, numLayers_hpm, numFeatures_hpm, lb, ub, samplingX, samplingY,
+                 activation=torch.tanh, activation_hpm=F.relu):
+        """
+        This function creates the components of the Neural Network and saves the datasets
+        :param x0: Position x at time zero
+        :param u0: Real Part of the solution at time 0 at position x
+        :param v0: Imaginary Part of the solution at time 0 at position x
+        :param tb: Time Boundary
+        :param X_f: Training Data for partial differential equation
+        :param layers: Describes the structure of Neural Network
+        :param lb: Value of the lower bound in space
+        :param ub: Value of the upper bound in space
+        """
+        torch.manual_seed(1234)
+        super().__init__(lb, ub, samplingX, samplingY, activation, noLayers=numLayers, noFeatures=numFeatures,
+                         use_singlenet=True, ssim_windowSize=9, initLayers=True, useGPU=True)
+
+        self.noLayers_hpm = numLayers_hpm
+        self.noFeatures_hpm = numFeatures_hpm
+        self.lin_layers_hpm = nn.ModuleList()
+        self.activation_hpm = activation_hpm
+
+        self.lb = torch.Tensor(lb).float().cuda()
+        self.ub = torch.Tensor(ub).float().cuda()
+
+        # build HPM
+        self.init_layers_hpm()
+
+    def init_layers_hpm(self):
+        """
+        This function creates the torch layers and initialize them with xavier
+        :param self:
+        :return:
+        """
+
+        self.lin_layers_hpm.append(nn.Linear(8, self.noFeatures_hpm))
+        for _ in range(self.noLayers_hpm):
+            self.lin_layers_hpm.append(nn.Linear(self.noFeatures_hpm, self.noFeatures_hpm))
+        self.lin_layers_hpm.append(nn.Linear(self.noFeatures_hpm, 2))
+
+        for m in self.lin_layers_hpm:
+            if isinstance(m, nn.Linear):
+                nn.init.xavier_normal_(m.weight)
+                nn.init.constant_(m.bias, 0)
+
+    def forward_hpm(self, x):
+        for i in range(0, len(self.lin_layers_hpm) - 1):
+            x = self.lin_layers_hpm[i](x)
+            x = self.activation_hpm(x)
+        x = self.lin_layers_hpm[-1](x)
+
+        return x
+
+    def net_pde(self, x, y, t, gamma=1.):
+        """
+        Calculates the quality of the pde estimation
+        :param x postion x
+        :param y postion y
+        :param t time t
+        """
+        u, u_yy, u_xx, u_t = self.net_uv(x, y, t) #netuv for derivatives
+
+        x = x.view(-1)
+        y = y.view(-1)
+
+        X = torch.stack([u_yy, u_xx], 1) #change parameters, Temperature, coordinates 
+        # X = torch.stack([x, y, u, v, u_yy, v_yy, u_xx, v_xx], 1)
+
+        f = u_t - self.forward_hpm(X)
+        # f = torch.stack([-1 * u_t, -1 * v_t], 1) - self.forward_hpm(X)
+
+        #f_u = f[:, 0]
+        # f_v = f[:, 1]
+
+        return u, f
+        # return u, v, f_u, f_v
+
+    def get_params(self, x, y, t): #not necessary
+
+        x = x.view(-1)
+        y = y.view(-1)
+        t = t.view(-1)
+
+        u, u_yy, u_xx, u_t = self.net_uv(x, y, t)
+        # u, v, u_yy, v_yy, u_xx, v_xx, u_t, v_t = self.net_uv(x, y, t)
+
+        X = torch.stack([x, y, u, u_yy, u_xx], 1)
+        # X = torch.stack([x, y, u, v, u_yy, v_yy, u_xx, v_xx], 1)
+
+        f = self.forward_hpm(X)
+        dudt = - f[:, 0]
+        # dvdt = - f[:, 1]
+
+        dudt = dudt.cpu().detach().numpy().reshape(-1, 1)
+        # dvdt = dvdt.cpu().detach().numpy().reshape(-1, 1)
+        # v_xx = v_xx.cpu().detach().numpy().reshape(-1, 1)
+        # v_yy = v_yy.cpu().detach().numpy().reshape(-1, 1)
+        u_xx = u_xx.cpu().detach().numpy().reshape(-1, 1)
+        u_yy = u_yy.cpu().detach().numpy().reshape(-1, 1)
+        u_t = u_t.cpu().detach().numpy().reshape(-1, 1)
+        # v_t = v_t.cpu().detach().numpy().reshape(-1, 1)
+
+        diff_u = np.linalg.norm(dudt - u_t, 2)
+        # diff_v = np.linalg.norm(dvdt-v_t,2)
+
+        reg_u = LinearRegression().fit([[u_xx[i][0], u_yy[i][0]] for i in range(len(u_xx))], dudt)  # ??
+        # reg_u = LinearRegression().fit([[v_xx[i][0],v_yy[i][0]] for i in range(len(v_xx))], dudt)
+        # reg_v = LinearRegression().fit([[u_xx[i][0],u_yy[i][0]] for i in range(len(u_xx))], dvdt)
+
+        lambda_u_xx, lambda_u_yy = reg_u.coef_[0]
+        # lambda_v_xx, lambda_v_yy = reg_u.coef_[0]
+        # lambda_u_xx, lambda_u_yy = reg_v.coef_[0]
+
+        return lambda_u_xx, lambda_u_yy, diff_u
+        # return lambda_v_xx, lambda_v_yy, lambda_u_xx, lambda_u_yy, diff_u, diff_v
+
+    def hpm_loss(self, x, y, t, Ex_u):
+        # def hpm_loss(self, x, y, t, Ex_u, Ex_v):
+        """
+        Returns the quality HPM net
+        """
+
+        x = x.view(-1)
+        y = y.view(-1)
+        t = t.view(-1)
+
+        u, f_u = self.net_pde(x, y, t)
+
+
+        Ex_u = Ex_u.view(-1)
+        # Ex_v = Ex_v.view(-1)
+
+        hpmLoss = torch.mean(f_u ** 2) + torch.mean((u - Ex_u) ** 2)
+        # hpmLoss = torch.mean(f_u ** 2) + torch.mean(f_v ** 2) + torch.mean((u - Ex_u) ** 2) + torch.mean((v - Ex_v) ** 2)
+        return hpmLoss
+
+def getDefaults():
+    # static parameter
+    nx = 200
+    ny = 200
+    nt = 1000
+    xmin = 0
+    xmax = 640
+    ymin = 0
+    ymax = 480
+    dt = 1
+    tmax = 1.9271e-04
+    #numOfEnergySamplingPointsX = 100
+    #numOfEnergySamplingPointsY = 100
+
+    coordinateSystem = {"x_lb": xmin, "x_ub": xmax, "y_lb": ymin, "y_ub" : ymax, "nx":nx , "ny":ny, "tmax": tmax, "dt": dt}
+
+    return coordinateSystem
+
+
+def loadTimesteps(pFile):
+
+    if not os.path.exists(pFile):
+        raise FileNotFoundError('Could not find file' + pFile)
+
+    hf = h5py.File(pFile, 'r')
+    timing = np.array(hf['timing'][:])
+
+    hf.close()
+    
+    timing = timing - np.min(timing)
+    
+    return timing
+
+
+if __name__ == "__main__":
+
+    # Initialize Horovod
+    hvd.init()
+
+    # Pin GPU to be used to process local rank (one GPU per process)
+    torch.cuda.set_device(hvd.local_rank())
+
+    parser = ArgumentParser()
+    parser.add_argument("--identifier", dest="identifier", type=str, default="S2D_DeepHPM")
+    parser.add_argument("--pData", dest="pData", type=str, default="/home/hoffma83/Code/PDElearning_Szymon/data/UKD/2014_022_rest.mat")  #"/home/h7/szch154b/2014_022_rest.mat")
+    parser.add_argument("--batchsize", dest="batchsize", type=int, default=307200)
+    parser.add_argument("--numbatches", dest="numBatches", type=int, default=500)
+    parser.add_argument("--numlayers", dest="numLayers", type=int, default=8)
+    parser.add_argument("--numfeatures", dest="numFeatures", type=int, default=300)
+    parser.add_argument("--numlayers_hpm", dest="numLayers_hpm", type=int, default=8)
+    parser.add_argument("--numfeatures_hpm", dest="numFeatures_hpm", type=int, default=300)
+    parser.add_argument("--t_ic", dest="t_ic", type=float, default=1e-7)
+    parser.add_argument("--t_pde", dest="t_pde", type=float, default=2e-7)
+    parser.add_argument("--pretraining", dest="pretraining", type=int, default=1)
+    args = parser.parse_args()
+
+    if hvd.rank() == 0:
+        print("-" * 10 + "-" * len(args.identifier) + "-" * 10)
+        print("-" * 10 + args.identifier + "-" * 10)
+        print("-" * 10 + "-" * len(args.identifier) + "-" * 10)
+
+    # set constants for training
+    coordinateSystem = getDefaults()
+    
+    maxTime = np.max(loadTimesteps(args.pData))
+    coordinateSystem["tMax"] = maxTime
+    print("tmax: %.2e" % (maxTime))
+    # fix maximum time based on timestep of our data
+
+
+
+    
+    modelPath = 'results/models/' + args.identifier + '/'
+    logdir = 'results/experiments/' + args.identifier + '/'
+    useGPU = True
+    activation = torch.tanh
+
+    # create modelpath
+    if hvd.rank() == 0:
+        pathlib.Path(modelPath).mkdir(parents=True, exist_ok=True)
+        # create logWriter
+    log_writer = SummaryWriter(logdir) if hvd.rank() == 0 else None
+
+    # create dataset
+    ds = HeatEquationHPMDataset(args.pData, coordinateSystem, args.numBatches, args.batchsize, shuffle=False, useGPU=True)
+    # Partition dataset among workers using DistributedSampler
+    train_sampler = torch.utils.data.distributed.DistributedSampler(ds, num_replicas=hvd.size(), rank=hvd.rank())
+    train_loader = torch.utils.data.DataLoader(ds, batch_size=1, sampler=train_sampler)
+
+    model = HeatEquationHPMNet(args.numLayers, args.numFeatures, args.numLayers_hpm, args.numFeatures_hpm, ds.lb, ds.ub,
+                              5, 5, activation).cuda()
+
+    optimizer = optim.Adam(model.parameters(), lr=5e-8)
+    optimizer = hvd.DistributedOptimizer(optimizer,
+                                         named_parameters=model.named_parameters(),
+                                         backward_passes_per_step=1)
+    
+    # broadcast parameters & optimizer state.
+    hvd.broadcast_parameters(model.state_dict(), root_rank=0)
+    hvd.broadcast_optimizer_state(optimizer, root_rank=0)
+
+    if args.pretraining:
+        """
+        approximate full simulation
+        """
+        print("Starting pretraining ..")
+        epoch = 0
+        l_loss = 1
+        l_loss_0 = 10
+        start_time = time.time()
+        while (l_loss > args.t_ic or abs(l_loss_0 - l_loss) > args.t_ic):
+            epoch += 1
+            for x, y, t, Ex_u in train_loader:
+                optimizer.zero_grad()
+                # calculate loss
+                loss = model.loss_ic(x, y, t, Ex_u)
+                loss.backward()
+                optimizer.step()
+            l_loss_0 = l_loss
+            l_loss = loss.item()
+
+            if (epoch % 100 == 0) and log_writer:
+                print("[%d] IC loss: %.4e [%.2fs]" % (epoch, l_loss, time.time() - start_time))
+                log_writer.add_scalar("loss_ic", l_loss, epoch)
+
+                writeValidationLoss(0, model, epoch, log_writer, coordinateSystem, identifier="PT")
+                writeIntermediateState(0, model, epoch, log_writer, coordinateSystem, identifier="PT")
+                writeValidationLoss(500, model, epoch, log_writer, coordinateSystem, identifier="PT")
+                writeIntermediateState(500, model, epoch, log_writer, coordinateSystem, identifier="PT")
+                writeValidationLoss(1000, model, epoch, log_writer, coordinateSystem, identifier="PT")
+                writeIntermediateState(1000, model, epoch, log_writer, coordinateSystem, identifier="PT")
+
+                save_checkpoint(model, modelPath + "0_ic/", epoch)
+
+    if args.pretraining:
+        save_checkpoint(model, modelPath + "0_ic/", epoch)
+    """
+    learn non-linear operator N 
+    """
+    # we need to significantly reduce the learning rate [default: 9e-6]
+    for paramGroup in optimizer.param_groups:
+        paramGroup['lr'] = 1e-7
+
+    l_loss = 1
+    start_time = time.time()
+    while (l_loss > args.t_pde):
+        epoch += 1
+        for x, y, t, Ex_u in train_loader:
+            # for x, y, t, Ex_u, Ex_v in train_loader:
+            optimizer.zero_grad()
+            loss = model.hpm_loss(x,
+                                  y,
+                                  t,
+                                  Ex_u)
+            """
+            loss = model.hpm_loss(x,
+                                  y,
+                                  t,
+                                  Ex_u,
+                                  Ex_v)
+            """
+            loss.backward()
+            optimizer.step()
+
+        l_loss = loss.item()
+
+        if (epoch % 1000 == 0) and log_writer:
+            lambda_u_xx, lambda_u_yy, diff_u = model.get_params(x, y, t)
+            # lambda_v_xx, lambda_v_yy, lambda_u_xx, lambda_u_yy, diff_u, diff_v = model.get_params(x, y, t)
+            # log_writer.add_scalar("lambda_v_xx", lambda_v_xx, epoch)
+            # log_writer.add_scalar("lambda_v_yy", lambda_v_yy, epoch)
+            log_writer.add_scalar("lambda_u_xx", lambda_u_xx, epoch)
+            log_writer.add_scalar("diff_u_t", diff_u, epoch)
+            # log_writer.add_scalar("diff_v_t", diff_v, epoch)
+            log_writer.add_scalar("hpm_loss", l_loss, epoch)
+
+            print("[%d] PDE loss: %.4e [%.2fs] saved" % (epoch, loss.item(), time.time() - start_time))
+
+            writeIntermediateState(0, model, epoch, log_writer, coordinateSystem, identifier="PDE")
+            writeIntermediateState(500, model, epoch, log_writer, coordinateSystem, identifier="PDE")
+            writeIntermediateState(1000, model, epoch, log_writer, coordinateSystem, identifier="PDE")
+
+            writeValidationLoss(0, model, epoch, log_writer, coordinateSystem, identifier="PDE")
+            writeValidationLoss(500, model, epoch, log_writer, coordinateSystem, identifier="PDE")
+            writeValidationLoss(1000, model, epoch, log_writer, coordinateSystem, identifier="PDE")
+
+            sys.stdout.flush()
+
+            log_writer.add_histogram('First Layer Grads', model.lin_layers_hpm[0].weight.grad.view(-1, 1), epoch)
+
+            save_checkpoint(model, modelPath + "1_pde/", epoch)
+
+    save_checkpoint(model, modelPath + "1_pde/", epoch)
+    print("--- converged ---")
---
+++ src/HeatEquation/HeatEquation_baseline_nohvd.py
@@ -0,0 +1,375 @@
+import numpy as np
+import time
+import torch
+import torch.nn as nn
+import torch.autograd
+import h5py
+import torch.optim as optim
+import scipy.io
+from torch.autograd import Variable
+import torch.optim as optim
+from enum import Enum
+from UKDDataset import SchrodingerEquationDataset
+import matplotlib.pyplot as plt
+import torch.utils.data.distributed
+from tensorboardX import SummaryWriter
+from argparse import ArgumentParser
+import os
+import sys
+import pathlib
+
+class HeatEquationBaseNet(nn.Module):
+    def __init__(self, lb, ub, samplingX, samplingY, activation=torch.tanh, noLayers = 8, noFeatures = 100, use_singlenet = True, ssim_windowSize = 9, initLayers = True, useGPU = False):
+        """
+        This function creates the components of the Neural Network and saves the datasets
+        :param x0: Position x at time zero
+        :param u0: Real Part of the solution at time 0 at position x
+        :param v0: Imaginary Part of the solution at time 0 at position x
+        :param tb: Time Boundary
+        :param X_f: Training Data for partial differential equation
+        :param layers: Describes the structure of Neural Network
+        :param lb: Value of the lower bound in space
+        :param ub: Value of the upper bound in space
+        """
+        torch.manual_seed(1234)
+        super(HeatEquationBaseNet, self).__init__()
+        self.activation = activation
+        self.useGPU = useGPU
+        self.lb = torch.Tensor(lb).float()
+        self.ub = torch.Tensor(ub).float()
+        if(self.useGPU):
+            self.lb = self.lb.cuda()
+            self.ub = self.ub.cuda()
+        self.noFeatures = noFeatures
+        self.noLayers = noLayers
+        self.in_t = nn.ModuleList([])
+        if(initLayers):
+            self.init_layers()
+
+
+    def init_layers(self):
+        """
+        This function creates the torch layers and initialize them with xavier
+        :param self:
+        :return:
+        """
+        print("layers", self.noLayers)
+        self.in_t.append(nn.Linear(3, self.noFeatures)) # time
+        for _ in range(self.noLayers):
+            self.in_t.append(nn.Linear(self.noFeatures, self.noFeatures))
+        self.in_t.append(nn.Linear(self.noFeatures,2))
+
+        
+        for m in self.in_t:
+            if isinstance(m, nn.Linear):
+                nn.init.xavier_normal_(m.weight)
+                nn.init.constant_(m.bias, 0)
+
+
+    def forward(self, x):
+        # scale spatiotemporral coordinate to [-1,1]
+        t_in = (x - self.lb)/(self.ub - self.lb)
+
+        # only normalize to [-1,1] in case of tanh
+        #if(self.activation == torch.tanh):
+        t_in = 2.0 * t_in - 1.0
+
+        for i in range(len(self.in_t)-1):
+            t_in = self.in_t[i](t_in)
+            t_in = self.activation(t_in)
+        return self.in_t[-1](t_in)
+                
+
+    def net_uv(self, x, y, t):
+        """
+        Function that calculates the nn output at postion (x,y) at time t
+        :param x: position
+        :param t: time
+        :return: Approximated solutions and their gradients
+        """
+
+        #torch.cuda.empty_cache()
+        dim = x.shape[0] #defines the shape of the gradient
+
+        # save input in variabeles is necessary for gradient calculation
+        x = Variable(x, requires_grad=True).cuda()
+        y = Variable(y, requires_grad=True).cuda()
+        t = Variable(t, requires_grad=True).cuda()
+
+        X = torch.stack([x, y, t], 1)
+
+        UV = self.forward(X)
+        u = UV[:, 0]
+        grads = torch.ones([dim]).cuda()
+
+        # huge change to the tensorflow implementation this function returns all neccessary gradients
+        J_U = torch.autograd.grad(u,[x,y,t], create_graph=True, grad_outputs=grads)
+        #J_V = torch.autograd.grad(v,[x,y,t], create_graph=True, grad_outputs=grads)
+    
+        u_x = J_U[0].reshape([dim])
+        u_y = J_U[1].reshape([dim])
+        u_t = J_U[2].reshape([dim])
+
+        #v_x = J_V[0].reshape([dim])
+        #v_y = J_V[1].reshape([dim])
+        #v_t = J_V[2].reshape([dim])
+
+        u_xx = torch.autograd.grad(u_x, x, create_graph=True, grad_outputs=grads)[0]
+        #v_xx = torch.autograd.grad(v_x, x, create_graph=True, grad_outputs=grads)[0]
+
+        u_yy = torch.autograd.grad(u_y, y, create_graph=True, grad_outputs=grads)[0]
+        #v_yy = torch.autograd.grad(v_y, y, create_graph=True, grad_outputs=grads)[0]
+
+        u_xx = u_xx.reshape([dim])
+        #v_xx = v_xx.reshape([dim])
+        u_yy = u_yy.reshape([dim])
+        #v_yy = v_yy.reshape([dim])
+
+        return u, u_yy, u_xx, u_t
+        #return u, v, u_yy, v_yy, u_xx, v_xx, u_t, v_t
+
+
+    def net_pde(self, x, y, t, omega=1.):
+        """
+        Calculates the quality of the pde estimation
+        :param x postion x
+        :param t time t
+        :param omega frequency of the harmonic oscillator 
+        """
+        #get predicted solution and the gradients 
+        u, u_yy, u_xx, u_t = self.net_uv(x, y, t)
+        #u, v, u_yy, v_yy, u_xx, v_xx, u_t, v_t = self.net_uv(x, y, t)
+        x = x.view(-1)
+        y = y.view(-1)
+
+        #calculate loss for real and imaginary part seperatly 
+        # fu is the real part of the schrodinger equation
+        #f_u = -1 * u_t   + omega * 0.5 *  (y ** 2) 
+        f_u = -1 * u_t - 0.5 * v_xx - 0.5 * v_yy + omega* 0.5 * (x ** 2) * v + omega * 0.5 *  (y ** 2) * v
+        # fv is the imaginary part of the schrodinger equation 
+        #f_v =  0.5 * u_xx + 0.5 * u_yy - omega* 0.5 * (x ** 2) * u - omega * 0.5 * (y ** 2) * u
+        f_v = -1 * v_t + 0.5 * u_xx + 0.5 * u_yy - omega* 0.5 * (x ** 2) * u - omega * 0.5 * (y ** 2) * u
+        return u, v, f_u #, f_v
+
+    
+    def loss_ic(self, x, y, t, u0, filewriter=None, epoch = 0, w_ssim = 0):
+    #def loss_ic(self, x, y, t, u0, v0, filewriter=None, epoch = 0, w_ssim = 0):
+        """
+        Returns the quality of the net
+        """
+        x = x.view(-1)
+        y = y.view(-1)
+        t = t.view(-1)
+        inputX = torch.stack([x, y, t], 1)
+        UV = self.forward(inputX)
+        u = UV[:, 0].view(-1,1)
+        #v = UV[:, 1].view(-1,1)
+
+        u0 = u0.view(-1,1)
+        #v0 = v0.view(-1,1)
+
+        #loss = (torch.mean((u0 - u) ** 2) + torch.mean((v0 - v) ** 2))
+        loss = (torch.mean((u0 - u)**2))
+
+        return loss
+
+    def loss_uv(self, x, y, t, u0, filewriter=None, epoch = 0, w_ssim = 0):
+    #def loss_uv(self, x, y, t, u0, v0, filewriter=None, epoch = 0, w_ssim = 0):
+        """
+	Returns the quality of the net
+        """
+        x = x.view(-1)
+        y = y.view(-1)
+        t = t.view(-1)
+        inputX = torch.stack([x, y, t], 1)
+        UV = self.forward(inputX)
+        u = UV[:, 0].view(-1,1)
+        #v = UV[:, 1].view(-1,1)
+
+        u0 = u0.view(-1,1)
+        #v0 = v0.view(-1,1)
+
+        loss_u = (torch.mean((u0 - u) ** 2)) 
+        #loss_v = (torch.mean((v0 - v) ** 2))
+
+        return loss_u  #, loss_v
+    
+    def loss_pde(self, x0, y0, t0, u0, xf, yf, tf, xe, ye, te, c, samplingX, samplingY,activateEnergyLoss=True, alpha=1.):
+    #def loss_pde(self, x0, y0, t0, u0, v0, xf, yf, tf, xe, ye, te, c, samplingX, samplingY,activateEnergyLoss=True, alpha=1.):
+        #reshape all inputs into correct shape 
+        x0 = x0.view(-1)
+        y0 = y0.view(-1)
+        t0 = t0.view(-1)
+        xf = xf.view(-1)
+        yf = yf.view(-1)
+        tf = tf.view(-1)
+        xe = xe.view(-1)
+        ye = ye.view(-1)
+        te = te.view(-1)
+        n0 = x0.shape[0]
+        nf = xf.shape[0]
+        inputX = torch.cat([x0, xf, xe])
+        inputY = torch.cat([y0, yf, ye])
+        inputT = torch.cat([t0, tf, te])
+
+        u, f_u = self.net_pde(inputX, inputY, inputT)
+        #u, v, f_u, f_v = self.net_pde(inputX, inputY, inputT)
+
+        solU = u[:n0]
+        #solV = v[:n0]
+
+        pdeLoss = alpha * torch.mean((solU - u0) ** 2) + \
+                  torch.mean(f_u ** 2) #+ \
+                  #alpha * torch.mean((solV - v0) ** 2) + \
+                  
+                  #torch.mean(f_v ** 2)
+
+        if activateEnergyLoss:
+            eU = u[n0 + nf:]
+            #eV = v[n0 + nf:]
+            eH = eU ** 2 #+ eV ** 2
+
+            lowerX = self.lb[0]
+            higherX = self.ub[0]
+
+            lowerY = self.lb[1]
+            higherY = self.ub[1]
+
+            disX = (higherX - lowerX) / samplingX
+            disY = (higherY - lowerY) / samplingY
+
+            u0 = u0.view(-1)
+            #v0 = v0.view(-1)
+            integral = 0.25 * disX * disY * torch.sum(eH * self.W) #weights???
+            # calculte integral over field for energy conservation
+            eLoss = (integral - c) ** 2
+            pdeLoss = pdeLoss + eLoss
+            
+        return pdeLoss
+
+
+def writeIntermediateState(timeStep, model, epoch, fileWriter,csystem, identifier = "PDE"):
+    """
+    Functions that write intermediate solutions to tensorboard
+    """
+    if fileWriter is None:
+        return 
+       
+    nx = csystem['nx']
+    ny = csystem['nx']
+
+    x, y, t = SchrodingerEquationDataset.getInput(timeStep,csystem)
+    x = torch.Tensor(x).float().cuda()
+    y = torch.Tensor(y).float().cuda()
+    t = torch.Tensor(t).float().cuda()
+
+    inputX = torch.stack([x, y, t], 1)
+    UV = model.forward(inputX).detach().cpu().numpy()
+
+    u = UV[:, 0].reshape((nx,ny))
+    
+    
+    #v = UV[:, 1].reshape((nx,ny))
+
+    #h = u ** 2 + v ** 2
+
+    fig = plt.figure()
+    plt.imshow(u, cmap='jet')
+    plt.colorbar()
+    fileWriter.add_figure('%s-real/t%.2f' % (identifier, t[0].cpu().numpy()), fig, epoch)
+    plt.close(fig)
+
+#    fig = plt.figure()
+#    plt.imshow(v, cmap='jet')
+#    plt.colorbar()
+#    fileWriter.add_figure('%s-imag/t%.2f' % (identifier, t[0].cpu().numpy()), fig, epoch)
+#    plt.close(fig)
+
+#    fig = plt.figure()
+#    plt.imshow(h, cmap='jet')
+#    plt.colorbar()
+#    fileWriter.add_figure('%s-norm/t%.2f' % (identifier, t[0].cpu().numpy()), fig, epoch)
+#    plt.close(fig)
+    
+    
+
+
+def valLoss(model, timeStep, csystem):
+    x, y, t = SchrodingerEquationDataset.getInput(timeStep,csystem)
+    x = torch.Tensor(x).float().cuda()
+    y = torch.Tensor(y).float().cuda()
+    t = torch.Tensor(t).float().cuda()
+
+    inputX = torch.stack([x, y, t], 1)
+    UV = model.forward(inputX).detach().cpu().numpy()
+    uPred = UV[:, 0].reshape(-1)
+    #vPred = UV[:, 1].reshape(-1)
+
+    # load label data
+    uVal, vVal = SchrodingerEquationDataset.getFrame(timeStep,csystem)
+    uVal = np.array(uVal).reshape(-1)
+    #vVal = np.array(vVal).reshape(-1)
+
+    valLoss_u = np.max(abs(uVal - uPred)) 
+    #valLoss_v = np.max(abs(vVal - vPred))
+    valSqLoss_u = np.sqrt(np.sum(np.power(uVal - uPred,2)))
+    #valSqLoss_v	= np.sqrt(np.sum(np.power(vVal - vPred,2)))
+
+    return valLoss_u, valSqLoss_u
+    #return valLoss_u, valLoss_v, valSqLoss_u, valSqLoss_v
+
+
+def writeValidationLoss(timeStep, model, epoch, writer, csystem, identifier):
+    if writer is None:
+        return
+        
+    _, _, t = SchrodingerEquationDataset.getInput(timeStep,csystem)
+    t = torch.Tensor(t).float().cuda()
+
+    valLoss_u, valSqLoss_u = valLoss(model, timeStep,csystem)
+    #valLoss_u, valLoss_v, valSqLoss_u, valSqLoss_v = valLoss(model, timeStep,csystem)
+    #valLoss_uv = valLoss_u + valLoss_v
+    #valSqLoss_uv = valSqLoss_u + valSqLoss_v
+    writer.add_scalar("inf: L_%s/u/t%.2f" % (identifier, t[0].cpu().numpy()), valLoss_u, epoch)
+    #writer.add_scalar("inf: L_%s/v/t%.2f" % (identifier, t[0].cpu().numpy()), valLoss_v, epoch)
+    #writer.add_scalar("inf: L_%s/uv/t%.2f" % (identifier, t[0].cpu().numpy()), valLoss_uv, epoch)
+    writer.add_scalar("2nd: L_%s/u/t%.2f" % (identifier, t[0].cpu().numpy()), valSqLoss_u, epoch)
+    #writer.add_scalar("2nd: L_%s/v/t%.2f" % (identifier, t[0].cpu().numpy()), valSqLoss_v, epoch)
+    #writer.add_scalar("2nd: L_%s/uv/t%.2f" % (identifier, t[0].cpu().numpy()), valSqLoss_uv, epoch)
+    
+
+
+def save_checkpoint(model, path, epoch):
+    #print(model.state_dict().keys())    
+    pathlib.Path(path).mkdir(parents=True, exist_ok=True) 
+    state = {
+        'model': model.state_dict(),
+    }
+    torch.save(state, path + 'model_' + str(epoch)+'.pt')
+    print("saving model to ---> %s" % (path + 'model_' + str(epoch)+'.pt'))
+
+    
+def load_checkpoint(model, path):
+    device = torch.device('cpu')
+    checkpoint = torch.load(path, map_location=device)
+    model.load_state_dict(checkpoint['model'])
+
+
+
+def getDefaults():
+    # static parameter
+    nx = 200
+    ny = 200
+    nt = 1000
+    xmin = -3
+    xmax = 3
+    ymin = -3
+    ymax = 3
+    dt = 0.001
+    tmax = 1
+    numOfEnergySamplingPointsX = 100
+    numOfEnergySamplingPointsY = 100
+
+    coordinateSystem = {"x_lb": xmin, "x_ub": xmax, "y_lb": ymin, "y_ub" : ymax, "nx":nx , "ny":ny, "nt": nt, "dt": dt}
+
+    return coordinateSystem, numOfEnergySamplingPointsX, numOfEnergySamplingPointsY, tmax 
