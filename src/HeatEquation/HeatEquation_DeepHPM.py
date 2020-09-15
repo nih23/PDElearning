some more bug fixes related to the normalisation

src/HeatEquation/HeatEquation_DeepHPM.py
@@ -66,7 +66,7 @@ class HeatEquationHPMNet(HeatEquationBaseNet):
         self.lin_layers_hpm.append(nn.Linear(8, self.noFeatures_hpm))
         for _ in range(self.noLayers_hpm):
             self.lin_layers_hpm.append(nn.Linear(self.noFeatures_hpm, self.noFeatures_hpm))
-        self.lin_layers_hpm.append(nn.Linear(self.noFeatures_hpm, 2))
+        self.lin_layers_hpm.append(nn.Linear(self.noFeatures_hpm, 1))
 
         for m in self.lin_layers_hpm:
             if isinstance(m, nn.Linear):
@@ -94,16 +94,10 @@ class HeatEquationHPMNet(HeatEquationBaseNet):
         y = y.view(-1)
 
         X = torch.stack([u_yy, u_xx], 1) #change parameters, Temperature, coordinates 
-        # X = torch.stack([x, y, u, v, u_yy, v_yy, u_xx, v_xx], 1)
 
         f = u_t - self.forward_hpm(X)
-        # f = torch.stack([-1 * u_t, -1 * v_t], 1) - self.forward_hpm(X)
-
-        #f_u = f[:, 0]
-        # f_v = f[:, 1]
 
         return u, f
-        # return u, v, f_u, f_v
 
     def get_params(self, x, y, t): #not necessary
 
@@ -166,13 +160,13 @@ class HeatEquationHPMNet(HeatEquationBaseNet):
 
 def getDefaults():
     # static parameter
-    nx = 200
-    ny = 200
+    nx = 640 
+    ny = 480
     nt = 1000
-    xmin = 0
-    xmax = 640
-    ymin = 0
-    ymax = 480
+    xmin = -1
+    xmax = 1
+    ymin = -1
+    ymax = 1
     dt = 1
     tmax = 1.9271e-04
     #numOfEnergySamplingPointsX = 100
@@ -256,7 +250,7 @@ if __name__ == "__main__":
     model = HeatEquationHPMNet(args.numLayers, args.numFeatures, args.numLayers_hpm, args.numFeatures_hpm, ds.lb, ds.ub,
                               5, 5, activation).cuda()
 
-    optimizer = optim.Adam(model.parameters(), lr=5e-8)
+    optimizer = optim.Adam(model.parameters(), lr=1e-5)
     optimizer = hvd.DistributedOptimizer(optimizer,
                                          named_parameters=model.named_parameters(),
                                          backward_passes_per_step=1)
@@ -300,6 +294,8 @@ if __name__ == "__main__":
 
     if args.pretraining:
         save_checkpoint(model, modelPath + "0_ic/", epoch)
+        
+
     """
     learn non-linear operator N 
     """

src/HeatEquation/HeatEquation_baseline_nohvd.py
@@ -53,11 +53,10 @@ class HeatEquationBaseNet(nn.Module):
         :param self:
         :return:
         """
-        print("layers", self.noLayers)
         self.in_t.append(nn.Linear(3, self.noFeatures)) # time
         for _ in range(self.noLayers):
             self.in_t.append(nn.Linear(self.noFeatures, self.noFeatures))
-        self.in_t.append(nn.Linear(self.noFeatures,2))
+        self.in_t.append(nn.Linear(self.noFeatures,1))
 
         
         for m in self.in_t:
@@ -69,11 +68,9 @@ class HeatEquationBaseNet(nn.Module):
     def forward(self, x):
         # scale spatiotemporral coordinate to [-1,1]
         t_in = (x - self.lb)/(self.ub - self.lb)
-
         # only normalize to [-1,1] in case of tanh
         #if(self.activation == torch.tanh):
         t_in = 2.0 * t_in - 1.0
-
         for i in range(len(self.in_t)-1):
             t_in = self.in_t[i](t_in)
             t_in = self.activation(t_in)
@@ -153,7 +150,6 @@ class HeatEquationBaseNet(nn.Module):
 
     
     def loss_ic(self, x, y, t, u0, filewriter=None, epoch = 0, w_ssim = 0):
-    #def loss_ic(self, x, y, t, u0, v0, filewriter=None, epoch = 0, w_ssim = 0):
         """
         Returns the quality of the net
         """
@@ -161,11 +157,8 @@ class HeatEquationBaseNet(nn.Module):
         y = y.view(-1)
         t = t.view(-1)
         inputX = torch.stack([x, y, t], 1)
-        UV = self.forward(inputX)
-        u = UV[:, 0].view(-1,1)
-        #v = UV[:, 1].view(-1,1)
-
-        u0 = u0.view(-1,1)
+        u = self.forward(inputX).view(-1)
+        u0 = u0.view(-1)
         #v0 = v0.view(-1,1)
 
         #loss = (torch.mean((u0 - u) ** 2) + torch.mean((v0 - v) ** 2))
@@ -173,27 +166,7 @@ class HeatEquationBaseNet(nn.Module):
 
         return loss
 
-    def loss_uv(self, x, y, t, u0, filewriter=None, epoch = 0, w_ssim = 0):
-    #def loss_uv(self, x, y, t, u0, v0, filewriter=None, epoch = 0, w_ssim = 0):
-        """
-	Returns the quality of the net
-        """
-        x = x.view(-1)
-        y = y.view(-1)
-        t = t.view(-1)
-        inputX = torch.stack([x, y, t], 1)
-        UV = self.forward(inputX)
-        u = UV[:, 0].view(-1,1)
-        #v = UV[:, 1].view(-1,1)
 
-        u0 = u0.view(-1,1)
-        #v0 = v0.view(-1,1)
-
-        loss_u = (torch.mean((u0 - u) ** 2)) 
-        #loss_v = (torch.mean((v0 - v) ** 2))
-
-        return loss_u  #, loss_v
-    
     def loss_pde(self, x0, y0, t0, u0, xf, yf, tf, xe, ye, te, c, samplingX, samplingY,activateEnergyLoss=True, alpha=1.):
     #def loss_pde(self, x0, y0, t0, u0, v0, xf, yf, tf, xe, ye, te, c, samplingX, samplingY,activateEnergyLoss=True, alpha=1.):
         #reshape all inputs into correct shape 
@@ -358,8 +331,8 @@ def load_checkpoint(model, path):
 
 def getDefaults():
     # static parameter
-    nx = 200
-    ny = 200
+    nx = 640
+    ny = 480 
     nt = 1000
     xmin = -3
     xmax = 3